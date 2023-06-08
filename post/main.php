63214e89a80a5c9b4d05acae17df3c9ad5fe3bf7
jcpascual1026 <130044225+jcpascual1026@users.noreply.github.com>
2023-06-08 19:41:20 +0800

    Update main.php
    Signed-off-by: jcpascual1026 <130044225+jcpascual1026@users.noreply.github.com>

--- post/main.php
+++ post/main.php
@@ -27,7 +27,7 @@ VALUES ('$name', '$message')";
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Code ex</title>
     <link rel="stylesheet" href="comment.css">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="post.css">
 </head>
 
 <body>
